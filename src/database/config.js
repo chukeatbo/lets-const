c31f9738c7728df708ad17abcbedaef1f19582c2
chukeatbo <chukeat.bo@gmail.com>
2023-01-15 00:24:29 +0700

    Fixed migration and seeder

---
+++ src/database/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    },
+    test: {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    },
+    production: {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    },
+}
